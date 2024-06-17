1ac6d2dac034bc7352ff057433304b6ffcbe8116
Kalos Lazo <lzm.kalos@gmail.com>
2024-06-17 10:55:24 -0500

    add hands controller

--- Assets/Scripts/DynamicTextureTiling.cs
+++ Assets/Scripts/DynamicTextureTiling.cs
@@ -1,46 +1,56 @@
-﻿/*
-	真主
-     穆罕默德 真主啊，为我们的大师穆罕默德和他的家人祈祷与和平 77 85 72 65 77 77 65 68 32 
-         83 104 97 104 122 97 105 98	
-*/
-using UnityEngine;
+﻿using UnityEngine;
 
 [ExecuteInEditMode]
 public class DynamicTextureTiling : MonoBehaviour
 {
-    // Reference to the original material with the texture
-     Material originalMaterial;
+    private Material materialInstance;
 
     void Start()
     {
-        // Ensure we have a material
-        originalMaterial = GetComponent<Renderer>().material;
-
-        // Create a new material instance for this object
-        Material materialInstance = new Material(originalMaterial);
-
-        // Apply the new material to the object
-        GetComponent<Renderer>().material = materialInstance;
-
-        // Get the initial scale of the object
-        Vector3 initialScale = transform.localScale;
-
-        // Set the texture tiling based on the initial scale
-        SetTextureTiling(materialInstance, initialScale);
+        InitMaterial();
     }
 
     void Update()
     {
-        // Adjust texture tiling based on the current scale
-        SetTextureTiling(GetComponent<Renderer>().material, transform.localScale);
+        // Ensure material is initialized before updating texture tiling
+        if (materialInstance == null)
+        {
+            InitMaterial();
+        }
+
+        if (materialInstance != null) {
+            SetTextureTiling(transform.localScale);
+        }
     }
 
-    void SetTextureTiling(Material material, Vector3 scale)
+    private void InitMaterial()
     {
-        // Calculate tiling based on the scale
-        Vector2 tiling = new Vector2(scale.x, scale.z);
+        Renderer renderer = GetComponent<Renderer>();
+        if (renderer != null && renderer.sharedMaterial != null)
+        {
+            if (materialInstance == null)
+            {
+                materialInstance = new Material(renderer.sharedMaterial);
+                renderer.material = materialInstance;
+            }
+        }
+    }
 
-        // Apply tiling to the material
-        material.mainTextureScale = tiling;
+    private void SetTextureTiling(Vector3 scale)
+    {
+        Vector2 tiling = new Vector2(scale.x, scale.z);
+        materialInstance.mainTextureScale = tiling;
+    }
+
+    private void OnDestroy()
+    {
+        if (Application.isEditor)
+        {
+            DestroyImmediate(materialInstance);
+        }
+        else
+        {
+            Destroy(materialInstance);
+        }
     }
 }
